5f5a74faf4e837bafd066dcd1fa3d74c1b2d5cb6
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-08-28 18:19:43 +0200

    Initial port of the BANCA database as a satellite package for bob

---
+++ xbob/db/banca/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <laurent.el-shafey@idiap.ch>
+
+"""The BANCA Database for face verification
+"""
+
+from .query import Database
+
+__all__ = ['Database']
+
