73e52e6a942f1dc075d3d3dc388bb2694b2ff867
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-06 13:17:04 +0200

    Update bootstrap and version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.banca',
-    version='1.1.1',
+    version='1.1.2',
     description='BANCA Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.banca',
     license='GPLv3',
